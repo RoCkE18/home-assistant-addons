179c34b0fceda03022e3feeeb61f189629b6bb74
RoCkE18 <78284374+RoCkE18@users.noreply.github.com>
2025-03-28 23:26:23 +0000

    Update run.sh

--- cloudflared_custom_v3/run.sh
+++ cloudflared_custom_v3/run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 echo "Iniciando tunnel Cloudflare..."
 echo "Token recebido: $TUNNEL_TOKEN"
+
+if [ -z "$TUNNEL_TOKEN" ]; then
+  echo "❌ ERRO: TUNNEL_TOKEN está vazio ou não foi passado pelo Supervisor!"
+  exit 1
+else
+  echo "✅ Token parece válido"
+fi
+
 cloudflared tunnel run --token "${TUNNEL_TOKEN}"
