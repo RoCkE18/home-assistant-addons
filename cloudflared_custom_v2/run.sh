5e861e5b603d4210dd1681dad8f718673cf1ba8d
RoCkE18 <78284374+RoCkE18@users.noreply.github.com>
2025-03-28 20:53:44 +0000

    Update run.sh

--- cloudflared_custom_v2/run.sh
+++ cloudflared_custom_v2/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Iniciando tunnel Cloudflare..."
 echo "Token recebido: $TUNNEL_TOKEN"
+echo "$TUNNEL_TOKEN" | grep -q "eyJ" && echo "Token parece válido" || echo "Token está vazio ou inválido"
 cloudflared tunnel run --token "${TUNNEL_TOKEN}"
